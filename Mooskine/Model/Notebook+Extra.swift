abcc46640e78c8e39b78c7709536975ff58452fc
akhilmantha <akhilmantha@live.com>
2018-07-15 12:46:08 +0530

    entity relationship and core data code generation

---
+++ Mooskine/Model/Notebook+Extra.swift
@@ -0,0 +1,14 @@
+//
+//  Notebook+Extra.swift
+//  Mooskine
+//
+//  Created by akhil mantha on 15/07/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+
+extension Notebook {
+    
+    
+}

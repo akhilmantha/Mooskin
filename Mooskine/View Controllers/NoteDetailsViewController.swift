6f166dcdfa3e7ee2bcd29719102464102a1aeba1
akhilmantha <akhilmantha@live.com>
2018-07-22 18:45:09 +0530

    Note details modified

--- Mooskine/View Controllers/NoteDetailsViewController.swift
+++ Mooskine/View Controllers/NoteDetailsViewController.swift
@@ -28,7 +28,7 @@ class NoteDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationItem.title = dateFormatter.string(from: note.creationDate)
+        navigationItem.title = dateFormatter.string(from: note.creationDate!)
         textView.text = note.text
     }
 
